40c4b60347d0c985525ca32f81b9c2e237eca9be
Matt Bugay <mjbugay@yahoo.com>
2020-09-21 19:28:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
- outer_results = []
+ outer_results = ""
  row_index = 0
  while row_index < src.count do
    element_index = 0
-   inner_results = []
+   inner_results = ""
    while element_index < src[row_index].count do
-     if src[row_index][element_index] == " "
+     if src[row_index][element_index] == ""
        inner_results << src[row_index][element_index]
      end
    element_index += 1
@@ -13,6 +13,6 @@ def join_nested_strings(src)
   outer_results << inner_results
   row_index += 1
 end
-return outer_results.to_s
+return outer_results
 end
 
